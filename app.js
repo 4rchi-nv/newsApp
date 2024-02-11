8b9577f76c7eb90207742733a1d400b8d5e8e565
Archie <agajanov0arslan@gmail.com>
2024-02-11 09:17:52 +0500

    news

--- app.js
+++ app.js
@@ -56,63 +56,8 @@ function customHttp() {
 // Init http module
 const http = customHttp();
 
-const newsService = (function() {
-  const apiKey = '1ac528f84b1e42b9b028ee2fd88ec6b4';
-  const apiUrl = 'https://newsapi.org/v2';
-
-  return {
-    topHeadlines(country = 'jp', cb) {
-      http.get(`${apiUrl}/top-headlines?country=${country}&category=technology&apiKey=${apiKey}`, cb);
-    },
-    everything(query, cb) {
-      http.get(`${apiUrl}/everything?q=${query}&apiKey=${apiKey}`, cb);
-    },
-  }
-})();
-
 //  init selects
 document.addEventListener('DOMContentLoaded', function() {
   M.AutoInit();
-  loadNews();
 });
 
-//load news function
-function loadNews() {
-  newsService.topHeadlines('jp', onGetResponse)
-}
-//function on get response from server
-function onGetResponse(err, res) {
-  renderNews(res.articles);
-  console.log(res.articles);
-}
-
-//function for rendering news
-function renderNews(news) {
-  const newsContainer = document.querySelector('.news-container .row');
-  let fragment = '';
-  news.forEach(element => {
-    const item = newsTemplate(element);
-    fragment += item;
-  });
-  newsContainer.insertAdjacentHTML('afterbegin', fragment);
-}
-
-//news item template function
-function newsTemplate({ urlToImage, title, url, description }) {
-  return `
-    <div class="col s12">
-      <div class="card">
-        <div class="card-image">
-          <img src='${urlToImage}'>
-          <span class='card-title'>${title || ''}</span>
-        </div>
-        <div class='card-content'>
-          <p>${description || ''}</p>
-        </div>
-        <div class='card-action'>
-          <a href='${url}'>Read more</a>
-        </div>
-      </div>
-    </div>
-  `
-}
